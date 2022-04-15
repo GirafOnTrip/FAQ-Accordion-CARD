e21f8d8d56487f23c5e878f8c60dfa962f471cd5
Venture00 <petitemanet@hotmail.fr>
2022-04-15 21:10:31 +0200

    V_2

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 const accordion = document.getElementsByClassName('accordion__item');
 
-console.log(accordion);
-
 for (i = 0; i < accordion.length; i++){
 
     accordion[i].addEventListener('click', function(){
